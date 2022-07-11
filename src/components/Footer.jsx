f9668ffa861bd5b51c63c1a64ae4c1981939f8f1
Octagony <moctagony@gmail.com>
2022-07-11 20:40:26 +0300

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import { AiFillGithub } from "react-icons/ai";
 const Footer = () => {
   return (
     <div className="w-full text-center mt-5  rounded-3xl">
-      <a href="https://github.com/octagony" target='_blank' rel="noreferrer">
-        <AiFillGithub className="mx-auto" size={35} />
+      <a className="inline-block" href="https://github.com/octagony" target='_blank' rel="noreferrer">
+        <AiFillGithub className="mx-auto" size={40} />
       </a>
       <p>Octagony / {new Date().getFullYear()}</p>
     </div>
