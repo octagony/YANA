521a0ff573a6a42fc4cd2740686d349b14c10a6c
Octagony <moctagony@gmail.com>
2023-01-20 21:36:01 +0300

    [2.0] Rewrite useTheme hook

--- src/store/useTheme.js
+++ src/store/useTheme.js
@@ -1,18 +1,15 @@
 import create from "zustand";
 
-const getInitialTheme = () => {
-  const theme = localStorage.getItem("theme") ?? "light";
-  return theme;
-};
-
 export const useTheme = create((set) => ({
-  theme: getInitialTheme(),
+  theme: localStorage.getItem("theme") ?? "light",
   setTheme: () =>
     set((state) => {
-      localStorage.setItem("theme", JSON.stringify(state.theme));
+      localStorage.setItem(
+        "theme",
+        JSON.stringify(state.theme === "light" ? "dark" : "light")
+      );
       return {
         theme: state.theme === "light" ? "dark" : "light",
       };
     }),
-  // set((state) => ({ theme: state.theme === "light" ? "dark" : "light" }));
 }));
