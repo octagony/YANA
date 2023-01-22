fab210956a4c0a4175adc3493a81a6e64930f218
Octagony <moctagony@gmail.com>
2023-01-22 15:32:57 +0300

    [2.0] Add styles to 404 page

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -4,7 +4,7 @@ import withLayout from "../layout/withLayout";
 import { useTheme } from "../store/useTheme";
 
 const Page404 = () => {
-  const theme = useTheme(state => state.theme)
+  const theme = useTheme((state) => state.theme);
 
   useEffect(() => {
     const root = document.documentElement;
@@ -16,12 +16,12 @@ const Page404 = () => {
   }, [theme]);
 
   return (
-    <div className="dark:text-white text-center py-4">
-      <h1 className="text-4xl">Wow, you find a treasure</h1>
-      <p>
-        Nah, just kidding, go back to{" "}
+    <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] dark:text-white text-center py-4 grid">
+      <h1 className="text-xl mb-4 sm:text-3xl">Wow, you find a treasure!</h1>
+      <p className="text-sm sm:text-xl">
+        Nah, just kidding, go back to
         <span className="text-emerald-700 dark:text-lime-300">
-          <Link to="/">main page!</Link>
+          <Link to="/"> main page!</Link>
         </span>
       </p>
     </div>
