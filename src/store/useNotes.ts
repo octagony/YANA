0a96fba1dd8ff0447fc595327489fa802ee9ea38
octagony <moctagony@gmail.com>
2023-07-17 20:20:09 +0300

    [REFACTOR] Added try/catch to useNotes hook in store

--- src/store/useNotes.ts
+++ src/store/useNotes.ts
@@ -1,43 +1,54 @@
-import create from "zustand";
-import { nanoid } from "nanoid";
-import { INotes, INote } from "../../types/INotes";
+import create from 'zustand'
+import { nanoid } from 'nanoid'
+import { INotes, INote } from '../../types/INotes'
 
-const getLocalStorage = (key: string) =>
-  JSON.parse(localStorage.getItem(key) || "[]");
+const getLocalStorage = (key: string) => {
+	try {
+		return JSON.parse(localStorage.getItem(key) || '[]')
+	} catch (error: unknown) {
+		console.log(`Error with get data from local storage: ${error}`)
+		return null
+	}
+}
 
 const setLocalStorage = (key: string, value: INote[]) => {
-  localStorage.setItem(key, JSON.stringify(value));
-};
+	try {
+		return localStorage.setItem(key, JSON.stringify(value))
+	} catch (error: unknown) {
+		console.log(`Error with get data from local storage: ${error}`)
+		return null
+	}
+}
 
-export const useNotes = create<INotes>((set) => ({
-  notes: getLocalStorage("notes-data"),
-  setNotes: (notes) => {
-    set(() => {
-      setLocalStorage("notes-data", notes);
-      return {
-        notes,
-      };
-    });
-  },
-  addNote: (text) =>
-    set((state) => ({
-      notes: [
-        { text, id: nanoid(), date: new Date().toLocaleDateString() },
-        ...state.notes,
-      ],
-    })),
-  deleteNote: (id) => {
-    set((state) => ({
-      notes: state.notes.filter((note) => note.id !== id),
-    }));
-  },
-  editNote: (id, value) => {
-    set((state) => ({
-      notes: state.notes.map((note) =>
-        note.id === id
-          ? { ...note, text: value, date: new Date().toLocaleDateString() }
-          : note
-      ),
-    }));
-  },
-}));
+export const useNotes = create<INotes>(set => ({
+	notes: getLocalStorage('notes-data'),
+	setNotes: notes => {
+		set(() => {
+			setLocalStorage('notes-data', notes)
+			return {
+				notes,
+			}
+		})
+	},
+	addNote: text =>
+		set(state => ({
+			notes: [
+				{ text, id: nanoid(), date: new Date().toLocaleDateString() },
+				...state.notes,
+			],
+		})),
+	deleteNote: id => {
+		set(state => ({
+			notes: state.notes.filter(note => note.id !== id),
+		}))
+	},
+	editNote: (id, value) => {
+		set(state => ({
+			notes: state.notes.map(note =>
+				note.id === id
+					? { ...note, text: value, date: new Date().toLocaleDateString() }
+					: note
+			),
+		}))
+	},
+}))
