6bd4c350155f7354705965c2fe214ee6090016bd
octagony <moctagony@gmail.com>
2023-07-19 19:38:37 +0300

    [FEAT] Supabase implementation process started

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useEffect } from 'react'
 import { useNotes } from '../store/useNotes'
 import NotesGrid from '../components/NotesGrid/NotesGrid'
@@ -6,9 +6,13 @@ import SearchBar from '../components/SearchBar/SearchBar'
 import withLayout from '../layout/withLayout'
 import { animated, useSpring } from '@react-spring/web'
 import { useThemeToggling } from '../hooks/useThemeToggling'
+import authService from '../services/auth/auth.service'
+import { User } from '@supabase/supabase-js'
 
 const App = () => {
 	const { notes, setNotes } = useNotes()
+	// const [session, setSession] = useState<Session | null>(null)
+	const [user, setUser] = useState<User | null>(null)
 
 	useThemeToggling()
 
@@ -23,8 +27,35 @@ const App = () => {
 		setNotes(notes)
 	}, [notes, setNotes])
 
+	// useEffect(() => {
+	// 	supabase.auth.getSession().then(({ data: { session } }) => {
+	// 		setSession(session)
+	// 	})
+
+	// 	const {
+	// 		data: { subscription },
+	// 	} = supabase.auth.onAuthStateChange((_event, session) => {
+	// 		setSession(session)
+	// 	})
+
+	// 	return () => subscription.unsubscribe()
+	// }, [])
+
+	const onSubmit = () => {
+		authService
+			.registerUser({
+				email: 'moctagony@gmail.com',
+				password: 'testpassword',
+			})
+			.then(data => setUser(data.data.user))
+			.finally(() => {
+				console.log(user)
+			})
+	}
+
 	return (
 		<animated.div style={animation}>
+			{/* <button onClick={onSubmit}>Click me</button> */}
 			<SearchBar />
 			<NotesGrid />
 		</animated.div>
