49a86a9103d4875fc01904f8da1ed28370c10516
octagony <moctagony@gmail.com>
2023-07-22 13:20:43 +0300

    [FEAT] Added auth page

--- src/layout/withLayout.tsx
+++ src/layout/withLayout.tsx
@@ -1,25 +1,21 @@
-import React, { ComponentType } from "react";
-import Footer from "../components/Footer/Footer";
-import Header from "../components/Header/Header";
-import styles from "./withLayout.module.css";
-import { ILayout } from "./withLayout.props";
-import { Provider } from "react-supabase";
-import { supabase } from "../services/auth/auth.helpers";
+import React, { ComponentType } from 'react'
+import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header'
+import styles from './withLayout.module.css'
+import { ILayout } from './withLayout.props'
 
 export function withLayout<T extends ILayout>(Component: ComponentType<T>) {
-  return (hocProps: T) => {
-    return (
-      <Provider value={supabase}>
-        <div className={styles.wrapper}>
-          <Header />
-          <div className={styles.inject}>
-            <Component {...(hocProps as T)} />
-          </div>
-          <Footer />
-        </div>
-      </Provider>
-    );
-  };
+	return (hocProps: T) => {
+		return (
+			<div className={styles.wrapper}>
+				<Header />
+				<div className={styles.inject}>
+					<Component {...(hocProps as T)} />
+				</div>
+				<Footer />
+			</div>
+		)
+	}
 }
 
-export default withLayout;
+export default withLayout
--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,9 +1,92 @@
 import React, { MouseEvent, useEffect, useState } from 'react'
+import withLayout from '../../layout/withLayout'
+import { useThemeToggling } from '../../hooks/useThemeToggling'
 
 const AuthPage = () => {
 	const [authState, setAuthState] = useState<'signup' | 'signin'>('signin')
 
-	return <div></div>
+	const toggleAuth = () => {
+		setAuthState(prev => (prev === 'signin' ? 'signup' : 'signin'))
+	}
+
+	useThemeToggling()
+
+	return (
+		<div>
+			<div className='grid place-items-center mx-2 my-20'>
+				<div
+					className='w-11/12 p-12 sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12 
+					px-6 py-10 sm:px-10 sm:py-6 
+					bg-white rounded-lg shadow-md lg:shadow-lg'
+				>
+					<h2 className='text-center font-semibold text-3xl lg:text-4xl text-gray-800'>
+						{authState === 'signup' ? 'Sign Up' : 'Login'}
+					</h2>
+
+					<form className='mt-10' method='POST'>
+						<label
+							htmlFor='email'
+							className='block text-xs font-semibold text-gray-600 uppercase'
+						>
+							E-mail
+						</label>
+						<input
+							id='email'
+							type='email'
+							name='email'
+							placeholder='e-mail address'
+							className='block w-full py-3 px-1 mt-2 
+									text-gray-800 appearance-none 
+									border-b-2 border-gray-100
+									focus:text-gray-500 focus:outline-none focus:border-gray-200'
+							required
+						/>
+
+						<label
+							htmlFor='password'
+							className='block mt-2 text-xs font-semibold text-gray-600 uppercase'
+						>
+							Password
+						</label>
+						<input
+							id='password'
+							type='password'
+							name='password'
+							placeholder='password'
+							className='block w-full py-3 px-1 mt-2 mb-4
+									text-gray-800 appearance-none 
+									border-b-2 border-gray-100
+									focus:text-gray-500 focus:outline-none focus:border-gray-200'
+							required
+						/>
+
+						<button
+							type='submit'
+							className='w-full py-3 mt-10 bg-gray-800 rounded-sm
+									font-medium text-white uppercase
+									focus:outline-none hover:bg-gray-700 hover:shadow-none'
+						>
+							{authState === 'signup' ? 'Sign Up' : 'Login'}
+						</button>
+
+						<div className='sm:flex sm:flex-wrap mt-8 sm:mb-4 text-sm text-center gap-2'>
+							<p className='flex-2'>
+								{authState === 'signup'
+									? 'Already have account?'
+									: 'Not account yet?'}
+							</p>
+							<span
+								className='flex-2 underline cursor-pointer'
+								onClick={toggleAuth}
+							>
+								{authState === 'signup' ? 'Sign in' : 'Sign up now'}
+							</span>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default AuthPage
+export default withLayout(AuthPage)
