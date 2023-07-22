4db93834133f46ae372c4b7d4743c98de379a721
octagony <moctagony@gmail.com>
2023-07-22 12:59:19 +0300

    [FEAT] Added .node-version file

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,36 +1,34 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
-import { useNotes } from "../store/useNotes";
-import NotesGrid from "../components/NotesGrid/NotesGrid";
-import SearchBar from "../components/SearchBar/SearchBar";
-import withLayout from "../layout/withLayout";
-import { animated, useSpring } from "@react-spring/web";
-import { useThemeToggling } from "../hooks/useThemeToggling";
-import { useRealtime } from "react-supabase";
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import { useNotes } from '../store/useNotes'
+import NotesGrid from '../components/NotesGrid/NotesGrid'
+import SearchBar from '../components/SearchBar/SearchBar'
+import withLayout from '../layout/withLayout'
+import { animated, useSpring } from '@react-spring/web'
+import { useThemeToggling } from '../hooks/useThemeToggling'
 
 const App = () => {
-  const { notes, setNotes } = useNotes();
+	const { notes, setNotes } = useNotes()
 
-  useThemeToggling();
+	useThemeToggling()
 
-  const animation = useSpring({
-    x: 0,
-    from: {
-      x: -300,
-    },
-  });
+	const animation = useSpring({
+		x: 0,
+		from: {
+			x: -300,
+		},
+	})
 
-  useEffect(() => {
-    setNotes(notes);
-  }, [notes, setNotes]);
+	useEffect(() => {
+		setNotes(notes)
+	}, [notes, setNotes])
 
-  return (
-    <animated.div style={animation}>
-      {/* <button onClick={onSubmit}>Click me</button> */}
-      <SearchBar />
-      <NotesGrid />
-    </animated.div>
-  );
-};
+	return (
+		<animated.div style={animation}>
+			<SearchBar />
+			<NotesGrid />
+		</animated.div>
+	)
+}
 
-export default withLayout(App);
+export default withLayout(App)
---
+++ src/pages/Auth/Auth.tsx
@@ -0,0 +1,9 @@
+import React, { MouseEvent, useEffect, useState } from 'react'
+
+const AuthPage = () => {
+	const [authState, setAuthState] = useState<'signup' | 'signin'>('signin')
+
+	return <div></div>
+}
+
+export default AuthPage
