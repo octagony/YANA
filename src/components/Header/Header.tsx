2587e090dcede23a6d6ce1ae609aec4c13645812
octagony <moctagony@gmail.com>
2023-07-29 23:14:59 +0300

    [REFACTOR] Update styles in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,41 +1,39 @@
-import React from 'react'
-import { Link } from 'wouter'
-import ThemeToggle from '../ThemeToggle/ThemeToggle'
-import styles from './Header.module.css'
-import { useAuthStore } from '../../store/auth.store'
-import useAuth from '../../hooks/useAuth'
+import { Link } from "wouter";
+import ThemeToggle from "../ThemeToggle/ThemeToggle";
+import styles from "./Header.module.css";
+import { useAuthStore } from "../../store/auth.store";
+import useAuth from "../../hooks/useAuth";
+import { HiOutlineLogout } from "react-icons/hi";
 
 const Header = () => {
-	const { user } = useAuthStore()
-	const { logout } = useAuth()
-	return (
-		<header className={styles.header}>
-			<div>
-				<Link to='/'>
-					<h1 className={styles.title}>YANA</h1>
-				</Link>
-				<span className={styles.subtitle}>Yet Another Notes App</span>
-				{user?.email && (
-					<span className='block text-center mt-2 text-sm md:text-base lg:text-lg'>
-						for {user?.email}
-					</span>
-				)}
-			</div>
-			<div className='flex flex-col-reverse items-center md:flex-row md:gap-4 sm:gap-4'>
-				{user?.email ? (
-					<div>
-						<button className='text-sm md:text-lg' onClick={logout}>
-							Logout
-						</button>
-					</div>
-				) : (
-					<Link href='/auth'>
-						<button>Login</button>
-					</Link>
-				)}
-				<ThemeToggle />
-			</div>
-		</header>
-	)
-}
-export default Header
+  const { user } = useAuthStore();
+  const { logout } = useAuth();
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__info}>
+        <Link to="/">
+          <h1 className={styles.title}>YANA</h1>
+        </Link>
+        <span className={styles.subtitle}>Yet Another Notes App</span>
+        {user?.email && (
+          <span className={styles.user__info}>for {user?.email}</span>
+        )}
+      </div>
+      <div className={styles.icons__wrapper}>
+        {user?.email ? (
+          <HiOutlineLogout
+            className={styles.logout__btn}
+            size={25}
+            onClick={logout}
+          />
+        ) : (
+          <Link href="/auth">
+            <button>Login</button>
+          </Link>
+        )}
+        <ThemeToggle />
+      </div>
+    </header>
+  );
+};
+export default Header;
