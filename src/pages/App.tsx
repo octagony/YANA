6ac1e04fb7cdd811db5a503bbfcce0370c0ff7a3
octagony <moctagony@gmail.com>
2023-08-03 21:37:30 +0300

    [FEAT] Test implement slider

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,8 +5,14 @@ import { animated, useSpring } from '@react-spring/web'
 import { useThemeToggling } from '../hooks/useThemeToggling'
 import { useAuthContext } from '../hooks/useAuthContext'
 import Loader from '../components/Loader/Loader'
+import Slider from 'react-slick'
+import { useNotes } from '../store/notes.store'
+
 const App = () => {
 	const { user } = useAuthContext()
+	const { notes } = useNotes()
+
+	const notesTitles = notes.map(note => note.text)
 
 	useThemeToggling()
 
@@ -17,8 +23,57 @@ const App = () => {
 		},
 	})
 
+	const settings = {
+		dots: false,
+		arrows: true,
+		infinite: false,
+		speed: 500,
+		slidesToShow: 10,
+		slidesToScroll: 2,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					arrows: false,
+				},
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					initialSlide: 2,
+					arrows: false,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					arrows: false,
+				},
+			},
+		],
+	}
+
 	return (
 		<>
+			<div className='px-4 py-2 mb-8'>
+				<Slider {...settings}>
+					{notesTitles.map(title => (
+						<div
+							className='p-2 text-center border border-red-50 dark:bg-gray-300 dark:text-neutral-800 rounded-full'
+							key={title}
+						>
+							{title.slice(0, 5)}
+						</div>
+					))}
+				</Slider>
+			</div>
+
 			{!user.email ? (
 				<Loader />
 			) : (
