7b252020f91096e4da5f3f918cc8f33b67d98fc4
octagony <moctagony@gmail.com>
2023-08-28 21:21:26 +0300

    [FEAT] Move styles in Auth page to module.css file

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,9 +1,22 @@
+// Libraries
 import { FormEvent, useState } from "react";
+
+// Components
 import withLayout from "../../layout/withLayout";
+
+// Hooks
 import { useThemeToggling } from "../../hooks/useThemeToggling";
-import { IUser } from "../../../types/IUser";
 import useAuth from "../../hooks/useAuth";
+
+// Types
+import { IUser } from "../../../types/IUser";
+
+// Store
 import { useAuthStore } from "../../store/auth.store";
+
+// Styles
+import styles from './Auth.module.css'
+
 
 const AuthPage = () => {
   const [authState, setAuthState] = useState<"signup" | "signin">("signin");
@@ -31,28 +44,26 @@ const AuthPage = () => {
 
   return (
     <div>
-      <div className="grid place-items-center mx-2 my-20">
+      <div className={styles.layout}>
         <div
-          className="w-9/12 p-12 sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12 
-					px-6 py-10 sm:px-10 sm:py-6 
-					bg-white rounded-lg shadow-md lg:shadow-lg"
+          className={styles.form__wrapper}
         >
-          <h2 className="text-center font-semibold text-3xl lg:text-4xl text-gray-800">
+          <h2 className={styles.form__header}>
             {authState === "signup" ? "Sign Up" : "Login"}
           </h2>
           {error && (
-            <div className="text-red-500 text-center mt-4 text-base">
+            <div className={styles.form__error}>
               {error}
             </div>
           )}
           <form
-            className="mt-10"
+            className={styles.form}
             method="POST"
             onSubmit={(event) => handleSubmit(event)}
           >
             <label
               htmlFor="email"
-              className="block text-xs font-semibold text-gray-600 uppercase"
+              className={styles.email__label}
             >
               E-mail
             </label>
@@ -61,10 +72,7 @@ const AuthPage = () => {
               type="email"
               name="email"
               placeholder="email"
-              className="block w-full py-3 px-1 mt-2 
-									text-gray-800 appearance-none 
-									border-b-2 border-gray-100
-									focus:text-gray-500 focus:outline-none focus:border-gray-200"
+              className={styles.email__input}
               required
               value={userData.email}
               onChange={(e) =>
@@ -74,7 +82,7 @@ const AuthPage = () => {
 
             <label
               htmlFor="password"
-              className="block mt-2 text-xs font-semibold text-gray-600 uppercase"
+              className={styles.password__label}
             >
               Password
             </label>
@@ -83,10 +91,7 @@ const AuthPage = () => {
               type="password"
               name="password"
               placeholder="password"
-              className="block w-full py-3 px-1 mt-2 mb-4
-									text-gray-800 appearance-none 
-									border-b-2 border-gray-100
-									focus:text-gray-500 focus:outline-none focus:border-gray-200"
+              className={styles.password__input}
               required
               value={userData.password}
               onChange={(e) =>
@@ -96,9 +101,7 @@ const AuthPage = () => {
 
             <button
               type="submit"
-              className="w-full py-3 mt-10 bg-gray-800 rounded-sm
-									font-medium text-white uppercase
-									focus:outline-none hover:bg-gray-700 hover:shadow-none"
+              className={styles.button}
               disabled={isLoading}
             >
               {isLoading
@@ -108,14 +111,14 @@ const AuthPage = () => {
                 : "Login"}
             </button>
 
-            <div className="sm:flex sm:flex-wrap mt-8 sm:mb-4 text-sm text-center gap-2">
-              <p className="flex-2">
+            <div className={styles.info__wrapper}>
+              <p className={styles.info__state}>
                 {authState === "signup"
                   ? "Already have account?"
                   : "Not account yet?"}
               </p>
               <span
-                className="flex-2 underline cursor-pointer"
+                className={styles.info__label}
                 onClick={toggleAuth}
               >
                 {authState === "signup" ? "Sign in" : "Sign up now"}
